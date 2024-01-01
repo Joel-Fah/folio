c6ad58c19ee093e01a65b93c6284368991a72ab2
Joël Fah <69576717+Joel-Fah@users.noreply.github.com>
2024-01-02 00:21:58 +0100

    Developement (#2)
    * feat: updated settings and env variables
    
    * fix: updated debug value

--- folio/settings.py
+++ folio/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', False).lower() == 'true'
+DEBUG = env('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
