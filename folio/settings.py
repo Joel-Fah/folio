59e103a6ef7edc59c56b90fb66234ef6b4472842
Joël Fah <69576717+Joel-Fah@users.noreply.github.com>
2024-01-06 20:06:28 +0100

    Update DEBUG default value settings.py

--- folio/settings.py
+++ folio/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG", "False")
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
