6a8a6806c8b530fb1d867ebe709bf9a5eeec84c6
Dejon Fah Joel Xavier <joelfah2003@gmail.com>
2024-01-09 05:12:20 +0100

    feat: connected front to back

--- folio/settings.py
+++ folio/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sitemaps',
     
     'core',
-    'django_quill',
+    'tinymce',
     'tailwind',
     'theme',
     'django_browser_reload',
@@ -103,12 +103,12 @@ WSGI_APPLICATION = 'folio.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'dev_sqlite': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # },
+    'dev_sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    },
     
-    'default' if DEBUG == True else 'dev': {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env("NAME"), 
         'USER': env("USER"),
@@ -118,7 +118,7 @@ DATABASES = {
     }
 }
 
-DATABASES['production' if DEBUG == False else 'default'] = dj_database_url.parse(env("DATABASE_URL"))
+DATABASES['production'] = dj_database_url.parse(env("DATABASE_URL"))
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -169,8 +169,31 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Add compression and caching support for whitenoise
 STORAGES = {
-    # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
+
+# TinyMCE
+TINYMCE_JS_URL = 'https://cdn.tiny.cloud/1/0ojmoo0qismstx45fnfsegx4ghvjn89ikerl6eqnb76r291b/tinymce/5/tinymce.min.js'
+TINYMCE_COMPRESSOR = False
+SECURE_REFERRER_POLICY = 'origin'
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "320px",
+    # "width": "960px",
+    'theme': "silver",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+    "language": "en_US",  # To force a specific language instead of the Django current language.
+}
+TINYMCE_SPELLCHECKER = True
