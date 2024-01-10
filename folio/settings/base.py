5f47e20ea89a1ae92b644549edf3428bc939740d
Joël Fah <69576717+Joel-Fah@users.noreply.github.com>
2024-01-10 15:12:47 +0100

    Development (#30)
    * feat: connected front to back
    
    * fix: changed tinymce default height and fixed tags names in template
    
    * fix: changed default database
    
    * fix: changed default database (#16)
    
    * fix: config databases based on DEBUG value for prod
    
    * fix: config databases based on DEBUG value for prod (#17)
    
    * fix: changed default database
    
    * fix: config databases based on DEBUG value for prod
    
    * fix: config databases based on DEBUG value for prod _ 2
    
    * fix: config databases based on DEBUG value for prod_3
    
    * fix: config databases based on DEBUG value for prod_4
    
    * fix: config databases based on DEBUG value for prod_5
    
    * feat: added settings config for dev and prod
    
    * feat: added DEBUG to env vars
    
    * feat: added custom 404 and 500 views and templates
    
    * Updated DEBUG for prod
    
    * Updated DEBUG for prod 2
    
    * fix: updated settings config
    
    * updated staticfiles path for dev and prod

--- folio/settings/base.py
+++ folio/settings/base.py
@@ -120,9 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'theme/static/'
-STATICFILES_DIRS = [
-    os.path.join('core/static/')
-]
 
 MEDIA_URL = '/media/'
 
