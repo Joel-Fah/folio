b7f32941077ba45baa8f1e90cc52e2b480463df1
Dejon Fah Joel Xavier <joelfah2003@gmail.com>
2024-01-11 14:49:43 +0100

    feat: added about pagewith two sections

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -18,14 +18,14 @@ document.addEventListener('DOMContentLoaded', function () {
 				currentPos < sectionTop + sectionHeight) { 
 				navbarLinks.forEach(function (navbarLink) { 
                     // If section is not visible
-					navbarLink.classList.remove('ring', 'bg-hoverColor/10', 'font-medium', 'md:bg-primaryColor/10', 'md:text-primaryColor', 'md:outline', 'md:outline-primaryColor/10');
+					navbarLink.classList.remove('ring', 'bg-hoverColor/10', 'font-medium', 'md:bg-primaryColor/10', 'md:text-primaryColor', 'md:outline-primaryColor');
 					navbarLink.classList.add('bg-transparent', 'max-sm:text-yellowWhite/70', 'md:hover:text-yellowWhite/70', 'md:hover:bg-yellowWhite/10', 'md:outline-transparent');
 				}); 
 
                 // Section is visible
 				document.querySelector('nav ul li a[href="#'
 					+ sectionId + '"]') 
-				.classList.add('ring', 'bg-hoverColor/10', 'font-medium', 'md:bg-primaryColor/10', 'md:text-primaryColor', 'md:outline', 'md:outline-primaryColor/10');
+				.classList.add('ring', 'bg-hoverColor/10', 'font-medium', 'md:bg-primaryColor/10', 'md:text-primaryColor', 'md:outline-primaryColor');
 
                 document.querySelector('nav ul li a[href="#'
 					+ sectionId + '"]') 
--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+import random
 from typing import Any
 from django.shortcuts import render
 from django.utils import timezone
@@ -22,6 +23,17 @@ class ProjectDetailView(DetailView):
     def get_queryset(self):
         return Project.objects.filter(id=self.kwargs['id'], slug=self.kwargs['slug'])
     
+class AboutView(TemplateView):
+    template_name = 'about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tool"] = random.choice(['angle-tool', 'hammer', 'tools', 'ruler-combine', 'design-nib', 'frame-alt-empty', 'crop', 'component'])
+        return context
+    
+    
+# Custom error pages (404, 500)
+
 def custom_page_not_found_view(request, exception):
     return render(request, '404.html', status=404)
 
