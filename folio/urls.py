5a9cd11ac2139e19ba2e86834e23311280d4fdde
Dejon Fah Joel Xavier <joelfah2003@gmail.com>
2024-02-14 15:09:13 +0100

    fix: fixed media files serving on prod

--- folio/urls.py
+++ folio/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps import views as sitemap_views
@@ -36,6 +37,12 @@ urlpatterns = [
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
 
 handler404 = 'core.views.custom_page_not_found_view'
 handler500 = 'core.views.custom_error_view'
