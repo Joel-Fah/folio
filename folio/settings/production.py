969a08948fae637ee263f994fdd4acd3c329dbfc
Dejon Fah Joel Xavier <joelfah2003@gmail.com>
2024-02-22 23:31:44 +0100

    feat: added AWS s3 bucket storage for media mstorage

--- folio/settings/production.py
+++ folio/settings/production.py
@@ -16,3 +16,14 @@ ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 DATABASES = {
     'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
 }
+
+# AWS S3 media storage management
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_SIGNATURE_NAME = os.getenv("AWS_S3_SIGNATURE_NAME"),
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
